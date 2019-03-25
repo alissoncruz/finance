a5cdd91629c9694096918cc6adaea684dc6e4a5b
czh0707 <czh0707@br.experian.com>
2019-03-24 22:16:58 -0300

    remove test

--- src/main/java/com/api/orchestrator/finance/controller/ExpenseController.java
+++ src/main/java/com/api/orchestrator/finance/controller/ExpenseController.java
@@ -1,30 +1,55 @@
 package com.api.orchestrator.finance.controller;
 
 import com.api.orchestrator.finance.api.v1.ExpenseApi;
-import com.api.orchestrator.finance.model.v1.ExpenseExpenseCodeTO;
-import com.api.orchestrator.finance.model.v1.ExpenseExpenseResponseTO;
-import com.api.orchestrator.finance.model.v1.ExpenseExpenseTO;
-import com.api.orchestrator.finance.model.v1.ExpenseUpdateExpenseTO;
+import com.api.orchestrator.finance.model.v1.*;
+import io.swagger.annotations.ApiParam;
+import org.owasp.esapi.ESAPI;
+import org.owasp.esapi.Logger;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+import static org.owasp.esapi.Logger.EVENT_UNSPECIFIED;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
+
 @RestController
 public class ExpenseController extends BaseRestController implements ExpenseApi {
 
+//    private final Logger log = ESAPI.getLogger(getClass());
+
     @Override
-    public ResponseEntity<ExpenseExpenseCodeTO> createExpense(@Valid ExpenseExpenseTO newExpense) {
-        return null;
+    public ResponseEntity<ExpenseExpenseCodeTO> createExpense(@Valid @RequestBody ExpenseExpenseTO newExpense) {
+//        log.info(EVENT_UNSPECIFIED, "Start method createExpense");
+//
+//        log.info(EVENT_UNSPECIFIED, "Finish method createExpense");
+        ExpenseExpenseCodeTO response = new ExpenseExpenseCodeTO();
+
+        response.setCode("0001");
+        return new ResponseEntity<>(response, CREATED);
     }
 
     @Override
-    public ResponseEntity<ExpenseExpenseResponseTO> getExpense(String code) {
-        return null;
+    public ResponseEntity<ExpenseResponseExpensesTO> getExpense(@PathVariable("code") String code) {
+
+//        log.info(EVENT_UNSPECIFIED, "Start method getExpense");
+//
+//        log.info(EVENT_UNSPECIFIED, "Finish method getExpense");
+        ExpenseResponseExpensesTO response = new ExpenseResponseExpensesTO();
+
+        return new ResponseEntity<>(response, OK);
     }
 
+
     @Override
-    public ResponseEntity<Void> updateExpense(String code, @Valid ExpenseUpdateExpenseTO expense) {
-        return null;
+    public ResponseEntity<Void> updateExpense(@PathVariable("code") String code,
+                                              @Valid @RequestBody ExpenseUpdateExpenseTO expense) {
+//        log.info(EVENT_UNSPECIFIED, "Start method updateExpense");
+//
+//        log.info(EVENT_UNSPECIFIED, "Finish method updateExpense");
+        return new ResponseEntity<>(OK);
     }
 }
